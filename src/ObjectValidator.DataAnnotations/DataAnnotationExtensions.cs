b1393da1a9852b889a373e106d47afaeddf73a23
Taylor Southwick <twsouthwick@outlook.com>
2018-07-02 20:10:03 -0700

    Add Data Annotations extension

---
+++ src/ObjectValidator.DataAnnotations/DataAnnotationExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ObjectValidator
+{
+    public static class DataAnnotationExtensions
+    {
+        public static ValidatorBuilder AddRegexConstraint(this ValidatorBuilder builder)
+        {
+            return builder
+                .AddRegexConstraint<RegularExpressionAttribute>(pattern => pattern.Pattern);
+        }
+    }
+}
