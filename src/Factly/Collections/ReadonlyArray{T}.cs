92e74f093c9b454436a5a3d1e2f90241de5b1f9c
Taylor Southwick <tasou@microsoft.com>
2018-10-19 12:10:26 -0700

    Add some tests

--- src/Factly/Collections/ReadonlyArray{T}.cs
+++ src/Factly/Collections/ReadonlyArray{T}.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Taylor Southwick. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System;
 using System.Diagnostics;
 
 namespace Factly.Collections
@@ -10,8 +11,18 @@ namespace Factly.Collections
     {
         private readonly T[] _array;
 
+        public ReadonlyArray(T[] array)
+            : this(array, array?.Length ?? 0)
+        {
+        }
+
         public ReadonlyArray(T[] array, int length)
         {
+            if (!IsValidLength(array, length))
+            {
+                throw new ArgumentOutOfRangeException(nameof(length));
+            }
+
             _array = array;
             Length = length;
         }
@@ -19,6 +30,28 @@ namespace Factly.Collections
         public int Length { get; }
 
         public Enumerator GetEnumerator() => new Enumerator(this);
+
+        private static bool IsValidLength(T[] array, int length)
+        {
+            if (length < 0)
+            {
+                return false;
+            }
+
+            if (array == null)
+            {
+                if (length == 0)
+                {
+                    return true;
+                }
+            }
+            else if (length <= array.Length)
+            {
+                return true;
+            }
+
+            return false;
+        }
 
         public struct Enumerator
         {
---
+++ test/Factly.Internal.Tests/ReadonlyArrayTests.cs
@@ -0,0 +1,97 @@
+﻿// Copyright (c) Taylor Southwick. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Factly.Collections;
+using System;
+using Xunit;
+
+namespace Factly.Internal.Tests
+{
+    public class ReadonlyArrayTests
+    {
+        [Fact]
+        public void NullArrayWithLength()
+        {
+            var array = new ReadonlyArray<object>(null, 0);
+
+            Assert.Equal(0, array.Length);
+
+            var enumerator = array.GetEnumerator();
+
+            Assert.False(enumerator.MoveNext());
+        }
+
+        [Fact]
+        public void NullArray()
+        {
+            var array = new ReadonlyArray<object>(null);
+
+            Assert.Equal(0, array.Length);
+
+            var enumerator = array.GetEnumerator();
+
+            Assert.False(enumerator.MoveNext());
+        }
+
+        [Fact]
+        public void InvalidLength()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => new ReadonlyArray<int>(null, 1));
+            Assert.Throws<ArgumentOutOfRangeException>(() => new ReadonlyArray<int>(null, -1));
+            Assert.Throws<ArgumentOutOfRangeException>(() => new ReadonlyArray<int>(new int[] { }, -1));
+            Assert.Throws<ArgumentOutOfRangeException>(() => new ReadonlyArray<int>(new int[] { }, 1));
+            Assert.Throws<ArgumentOutOfRangeException>(() => new ReadonlyArray<int>(new[] { 1 }, 2));
+        }
+
+        [Fact]
+        public void DefaultArray()
+        {
+            var array = default(ReadonlyArray<object>);
+
+            Assert.Equal(0, array.Length);
+
+            var enumerator = array.GetEnumerator();
+
+            Assert.False(enumerator.MoveNext());
+        }
+
+        [Fact]
+        public void SanityArray()
+        {
+            var array = new ReadonlyArray<int>(new[] { 1, 2, 3 });
+
+            Assert.Equal(3, array.Length);
+
+            var enumerator = array.GetEnumerator();
+
+            Assert.True(enumerator.MoveNext());
+            Assert.Equal(1, enumerator.Current);
+
+            Assert.True(enumerator.MoveNext());
+            Assert.Equal(2, enumerator.Current);
+
+            Assert.True(enumerator.MoveNext());
+            Assert.Equal(3, enumerator.Current);
+
+            Assert.False(enumerator.MoveNext());
+        }
+
+        [Fact]
+        public void TruncatedArray()
+        {
+            var array = new ReadonlyArray<int>(new[] { 1, 2, 3 }, 2);
+
+            Assert.Equal(2, array.Length);
+
+            var enumerator = array.GetEnumerator();
+
+            Assert.True(enumerator.MoveNext());
+            Assert.Equal(1, enumerator.Current);
+
+            Assert.True(enumerator.MoveNext());
+            Assert.Equal(2, enumerator.Current);
+
+            Assert.False(enumerator.MoveNext());
+        }
+    }
+}
