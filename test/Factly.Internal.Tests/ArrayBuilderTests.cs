d8f1e97799899a99b6f662cf708ea7c84f3ea3a9
Taylor Southwick <tasou@microsoft.com>
2018-10-19 12:46:45 -0700

    Add array builder test (#16)

---
+++ test/Factly.Internal.Tests/ArrayBuilderTests.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (c) Taylor Southwick. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Factly.Collections;
+using System;
+using Xunit;
+
+namespace Factly.Internal.Tests
+{
+    public class ArrayBuilderTests
+    {
+        [Fact]
+        public void DefaultFailsToAdd()
+        {
+            var builder = default(ArrayBuilder<int>);
+
+            Assert.Throws<IndexOutOfRangeException>(() => builder.Add(0));
+        }
+
+        [Fact]
+        public void DefaultGetsEmpty()
+        {
+            var builder = default(ArrayBuilder<int>);
+            var array = builder.Build();
+
+            Assert.Equal(0, array.Length);
+        }
+
+        [Fact]
+        public void SingleItem()
+        {
+            var builder = new ArrayBuilder<int>(5);
+            builder.Add(3);
+            var array = builder.Build();
+
+            Assert.Equal(1, array.Length);
+            var enumerator = array.GetEnumerator();
+            Assert.True(enumerator.MoveNext());
+            Assert.Equal(3, enumerator.Current);
+        }
+    }
+}
